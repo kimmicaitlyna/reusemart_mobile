8e7182715b625160fd6bdab9378383d3c0a769a0
Vantekk78 <220712063@students.uajy.ac.id>
2025-06-09 17:27:34 +0700

    first

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -49,6 +49,8 @@ flutter {
 
 }
 
-    dependencies {
+dependencies {
     coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.0.3")
+    implementation(platform("com.google.firebase:firebase-bom:33.15.0"))
+    implementation("com.google.firebase:firebase-analytics")
 }
