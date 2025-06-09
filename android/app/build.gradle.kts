f849a4aace8909c893010585a84b00915e48a4b3
Vantekk78 <220712063@students.uajy.ac.id>
2025-06-09 18:25:28 +0700

    yer

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,10 +1,12 @@
 plugins {
     id("com.android.application")
+
     // START: FlutterFire Configuration
     id("com.google.gms.google-services")
+
     // END: FlutterFire Configuration
-    id("kotlin-android")
-    // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
+    id("kotlin-android") version "1.9.22"
+
     id("dev.flutter.flutter-gradle-plugin")
 }
 
